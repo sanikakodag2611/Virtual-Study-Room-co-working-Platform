d9dbbb9b8650f63a74924b9ac33c90b55efd85a9
sanikakodag2611 <kodagsanika2611@gmail.com>
2026-02-25 22:15:35 +0530

    Initial commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require("../models/User");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
